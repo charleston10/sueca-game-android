c5c5c9e348b6ce0c428230a4c229c0189564f61b
Charleston Anjos <charleston.anjos>
2018-07-27 16:03:31 -0300

    add animation scale

--- Sueca/app/src/main/java/com/charleston/sueca/MainActivity.kt
+++ Sueca/app/src/main/java/com/charleston/sueca/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.charleston.sueca
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity(), QuestionView {
 
@@ -26,7 +27,12 @@ class MainActivity : AppCompatActivity(), QuestionView {
 
     override fun showQuestion(questionData: QuestionData) {
         question_txt_description.text = questionData.description
-        question_txt_explanation.text = questionData.explanation
+        question_txt_description.scaleCenter()
+
+        if (questionData.explanation.isNotEmpty()) {
+            question_txt_explanation.text = questionData.explanation
+            question_txt_explanation.scaleCenter()
+        }
     }
 
     private fun bindElements() {
--- Sueca/app/src/main/java/com/charleston/sueca/ViewExt.kt
+++ Sueca/app/src/main/java/com/charleston/sueca/ViewExt.kt
@@ -1,6 +1,8 @@
 package com.charleston.sueca
 
 import android.view.View
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 fun View.show() {
     this.visibility = View.VISIBLE
@@ -8,4 +10,11 @@ fun View.show() {
 
 fun View.hide() {
     this.visibility = View.GONE
+}
+
+fun View.scaleCenter() {
+    val animation = AnimationUtils.loadAnimation(context, R.anim.scale)
+    animation.reset()
+    clearAnimation()
+    startAnimation(animation)
 }
