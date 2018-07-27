7a0040f44c6bcaf28a37baf2b6d1edfd03a4337c
Charleston Anjos <charleston.anjos>
2018-07-27 13:49:25 -0300

    add explanation question

--- Sueca/app/src/main/java/com/charleston/sueca/MainActivity.kt
+++ Sueca/app/src/main/java/com/charleston/sueca/MainActivity.kt
@@ -2,6 +2,7 @@ package com.charleston.sueca
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), QuestionView {
 
@@ -10,14 +11,25 @@ class MainActivity : AppCompatActivity(), QuestionView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bindElements()
     }
 
     override fun showLoading() {
+        question_btn_play.hide()
+        question_container.hide()
     }
 
     override fun hideLoading() {
+        question_container.show()
+        question_btn_play.show()
     }
 
     override fun showQuestion(questionData: QuestionData) {
+        question_txt_description.text = questionData.description
+        question_txt_explanation.text = questionData.explanation
+    }
+
+    private fun bindElements() {
+        question_btn_play.setOnClickListener { questionPresenter.sort() }
     }
 }
